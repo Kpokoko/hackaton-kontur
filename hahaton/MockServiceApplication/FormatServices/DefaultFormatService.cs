6e4c88a23121e9dc90708cb66de6c35257755543
Vladimir Golubev <gor.prast@bk.ru>
2026-03-31 14:21:20 +0500

    add DefaultFormatService

---
+++ hahaton/MockServiceApplication/FormatServices/DefaultFormatService.cs
@@ -0,0 +1,26 @@
+using System.Text;
+
+namespace MockServiceApplication.FormatServices;
+
+public class DefaultFormatService : IFormatService<string>
+{
+    public Random Random { get; }
+    
+    public DefaultFormatService(Random random)
+    {
+        Random = random;
+    }
+    
+    public string Generate()
+    {
+        var result = new StringBuilder();
+        const string chars = "abcdefghijklmnopqrstuvwxyZ0123456789";
+        
+        for (var i = 0; i < Random.Next(1, 30); i++)
+        {
+            result.Append(chars[Random.Next(chars.Length)]);
+        }
+        
+        return result.ToString();
+    }
+}
